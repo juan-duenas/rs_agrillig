0e15655b728e9e139224d2e189c512d899bfab4e
Juan F. Dueñas <62545646+juan-duenas@users.noreply.github.com>
2025-09-05 16:42:10 +0200

    Modification to script. First supplementary figure and table

--- Analysis.r
+++ Analysis.r
@@ -5,9 +5,9 @@ pkgs <- c("tidyverse", "mgcv", "gratia", "ggpubr", "vegan", "phyloseq", "ggordip
 
 vapply(pkgs, FUN = library, FUN.VALUE = logical(1L), logical.return = TRUE, character.only = TRUE)
 
-# Figure 1 ------ Control soil
+# Figure 1 ------ Control soil ####
 
-## load data, pivot and store on a list
+## load data, pivot and store on a list 
 rs <- read_delim("rs_db.csv", delim = ",") %>% 
        filter(treatment%in%"Ctrl")%>%
        pivot_longer(
@@ -32,56 +32,74 @@ alf <- cbind(sample_data(f)[,c(1)], estimate_richness(f, measures=c("InvSimpson"
        right_join(.,rs[["cf"]], by="potID")%>% #fixed 
        select(-c(y, var.y))%>%
        rename(var=var.x, y=InvSimpson)%>%
-       select(1,4,5,6,3,2) %>% as_tibble(.)
+       select(1,4,5,6,3,2) %>% 
+       filter(!is.na(y)) %>% as_tibble(.) %>%
+       mutate_at('potID', as.integer) 
 
 rs$alf <- alf # append to db list
 rm(alf)
 
-# beta diversity (will not go on the list)
-betf <- vegdist(otu_table(f), method = "jac", binary = T)%>%
-        metaMDS(., parallel=2, trace=F, weakties=T)
+# Model selection routine
 
-f_tr <- sample_data(f)[,c(1,3,4)] %>% data.frame(.) %>% rename(dl=moistureL, z=whc) %>%
-        mutate_at('dl', as.factor)
-        
-ford <- gg_ordiplot(betf, groups = f_tr$dl, hull = F, label = F,
-                    spiders = F, ellipse = F, plot = F, choices = c(1, 2), scaling=1)%>%
-         pluck('df_ord')%>%
-         rownames_to_column("potID")%>%
-         mutate_at('potID', as.numeric)%>%
-         left_join(.,f_tr, by="potID") %>% select(-Group)
-
-rm(betf)
-
-# function for Models (GAMS)
-
-Mymodsc=function(df, fml){
-        # fits a GAM to each dataset on a list in df with max 3 base functions
-        # returns a list of named gams.
-        # argument fml is a string list stating the family of the conditional prob. and must be provided separately
-        md=list(gm=gam(data = df, formula = y~s(x, bs="tp", k=3), family = fml, method="REML"))#,
-                #linear=gam(data = df, formula = y~x, family = fml, method="REML"))
+Mymodselc <- function(df, fml) {
+  # function to set two pairs of models one linear one gam  
+  set1 <- list(
+                 gm     = gam(y ~ s(x, bs = "tp", k = 3), data = df, family = fml),
+                 linear = lm(y ~ x, data = df)
+                )
 }
 fml <- list('gaussian', 'gaussian', 'poisson',
             'betar', 'gaussian')
+models <- Map(Mymodselc, rs, fml) # iteration of model fit
 
-mds <- mapply(Mymodsc, rs, fml) # iteratively apply Mymodsc
+# function to extract AIC and rank models
+extract_model_selection_table <- function(model_list) {
+        out <- lapply(names(model_list), function(ds) {
+        sublist <- model_list[[ds]]
+    
+         # extract AICs
+          aics <- sapply(sublist, AIC)
+    
+         # ΔAIC
+        delta <- aics - min(aics)
+    
+         # Akaike weights
+         weights <- exp(-0.5 * delta)
+         weights <- weights / sum(weights)
+    
+        # build selection table
+        tab <- data.frame(
+               Dataset = ds,
+               Model = names(aics),
+               AIC = aics,
+               DeltaAIC = delta,
+               Weight = weights,
+               Rank = rank(aics, ties.method = "first")
+              )
+    
+        tab[order(tab$AIC), ]
+  })
+  
+  # combine into one big table
+  do.call(rbind, out)
+}
 
-# Mymodsc <- function(df, fml) {
-#   # make sure family is turned into a family object
-#   #fam <- match.fun(fml)()
-#   
-#   # first set of models
-#   set1 <- list(
-#     gm     = gam(y ~ s(x, bs = "tp", k = 3), data = df, family = fml, method = "REML"),
-#     linear = gam(y ~ x, data = df, family = fml, method = "REML")
-#   )
-#   
-#    # return nested list
-#   list(set1 = set1)
-# }
 
+myAICs <- extract_model_selection_table(models)
 
+# function for Models (GAMS)
+
+Mygamsc=function(df, fml){
+  # fits a GAM to each dataset on a list in df with max 3 base functions
+  # returns a list of named gams.
+  # argument fml is a string list stating the family of the conditional prob. and must be provided separately
+  md=list(gm=gam(data = df, formula = y~s(x, bs="tp", k=3), family = fml, method="REML"))
+}
+
+fml <- list('gaussian', 'gaussian', 'poisson',
+            'betar', 'gaussian')
+
+mds <- mapply(Mygamsc, rs, fml) # iteratively apply Mygamsc
 
 sm <- lapply(mds, summary)%>% # get each model summary
       lapply(., '[[', 's.table')%>% # extract each s.table
@@ -108,7 +126,7 @@ appraise(mds$wsa.gm, method = "simulate", n_simulate = 1000)
 appraise(mds$alf.gm, method = "simulate", n_simulate = 1000)
 
 # Multivariate Model (PERMANOVA)
-adonis2(otu_table(f)~f_tr$dl, permutations = 999, method = 'jac', binary=T, by="margin")
+adonis2(otu_table(f)~f_tr$dl, permutations = 9999, method = 'jac', binary=T, by="margin")
 
 ## Plots
 # estimate uncertainty around fits
@@ -149,6 +167,23 @@ pl <- mapply(Myplotsc, rs, fml, laby, se)
 
 
 #plot ord
+
+# beta diversity (will not go on the list)
+betf <- vegdist(otu_table(f), method = "jac", binary = T)%>%
+  metaMDS(., parallel=2, trace=F, weakties=T)
+
+f_tr <- sample_data(f)[,c(1,3,4)] %>% data.frame(.) %>% rename(dl=moistureL, z=whc) %>%
+  mutate_at('dl', as.factor)
+
+ford <- gg_ordiplot(betf, groups = f_tr$dl, hull = F, label = F,
+                    spiders = F, ellipse = F, plot = F, choices = c(1, 2), scaling=1)%>%
+  pluck('df_ord')%>%
+  rownames_to_column("potID")%>%
+  mutate_at('potID', as.numeric)%>%
+  left_join(.,f_tr, by="potID") %>% select(-Group)
+
+rm(betf)
+
 ord <- ggplot() + 
             geom_vline(xintercept=0.0, color="White", linewidth=1, linetype=1)+
             geom_hline(yintercept=0.0, color="White", linewidth=1, linetype=1)+    
@@ -167,4 +202,220 @@ pl$ord <- ord # append ordination plot to list
 (fig1 <- ggarrange(pl$mwd, pl$wsa, pl$imp, pl$cf, pl$alf, pl$ord, ncol = 3, nrow = 2,
                   common.legend = T, legend = "right", labels = c('a', 'b', 'c', 'd', 'e', 'f')))
 
+rm(list = setdiff(ls(), c("fig.1")))
 
+# Figure 1 sup ------ multiple factor effects ####
+
+# load data and select all treatments except for control
+
+'%notin%' <- Negate('%in%')
+
+rs <- read_delim("rs_db.csv", delim = ",") %>% 
+       filter(treatment %notin% "All_in")%>%
+       mutate_at(c('no_poll', "treatment"), as.factor)%>%
+       mutate(treatment=fct_relevel(treatment, c("Ctrl", "Copper","µPlastic", "Nitrogen", "Salinity", "Surfactant")))%>%   
+       pivot_longer(
+       cols = 7:10,
+       names_to = "var",
+       values_to = "y")%>%
+       select(potID, dl, whc, treatment, no_poll, var, y)%>%
+       rename(x=dl, z=whc, p=no_poll, t=treatment)%>%
+       filter(!is.na(y))%>%
+       group_split(var)%>% # split into several databases by treatment and store on a list
+       set_names(., nm = c("cf", "mwd", "imp", "wsa"))%>% #give names to each set  
+       as.list(.)
+
+# community data
+f <- readRDS("RS_all.rds")%>% 
+     subset_taxa(., Kingdom=="Fungi")%>% # eliminate non target organisms
+     filter_taxa(., function(x) sum(x) > 0, TRUE)%>% # keep variants with more than 0 reads
+     subset_samples(., treatment%notin%c("all"))
+
+# alpha diversity
+alf <- cbind(sample_data(f)[,c(1)], estimate_richness(f, measures=c("InvSimpson")))%>%
+       mutate(var=rep("invsimp",255))%>%
+       right_join(.,rs[["cf"]], by="potID")%>% #fixed 
+       select(-c(y, var.y))%>%
+       rename(var=var.x, y=InvSimpson)%>%
+       select(1,4,5,6,7,3,2) %>%
+       filter(!is.na(y)) %>% as_tibble(.) %>%
+       mutate_at('potID', as.integer) 
+
+rs$alf <- alf # append to db list
+rm(alf)
+
+# Model selection
+
+Mymodselc <- function(df, fml) {
+  # function to set two pairs of models one linear one gam  
+  gms <- list(
+    gm1 = gam(data = df, formula = y~ t + s(x, t, bs = 'fs', k=3), family = fml, method="ML", select = T),
+    gm2 = gam(data = df, formula = y~ s(x, t, bs = 'fs', k=3), family = fml, method="ML", select = T)
+  )
+}
+
+fml <- list('gaussian', 'gaussian', 'poisson',
+            'betar', 'gaussian')
+gmssel <- Map(Mymodselc, rs, fml) # iteration of model fit
+
+# function to extract AIC and rank models
+extract_model_selection_table <- function(model_list) {
+  out <- lapply(names(model_list), function(ds) {
+    sublist <- model_list[[ds]]
+    
+    # extract AICs
+    aics <- sapply(sublist, AIC)
+    
+    # ΔAIC
+    delta <- aics - min(aics)
+    
+    # Akaike weights
+    weights <- exp(-0.5 * delta)
+    weights <- weights / sum(weights)
+    
+    # build selection table
+    tab <- data.frame(
+      Dataset = ds,
+      Model = names(aics),
+      AIC = aics,
+      DeltaAIC = delta,
+      Weight = weights,
+      Rank = rank(aics, ties.method = "first")
+    )
+    
+    tab[order(tab$AIC), ]
+  })
+  
+  # combine into one big table
+  do.call(rbind, out)
+}
+
+myAICs <- extract_model_selection_table(gmssel)
+# differences are minimal, chose GAM formula 1
+
+# function for Models (GAMS)
+
+Mygamsc=function(df, fml){
+  # fits a GAM to each dataset on a list in df with max 3 base functions
+  # returns a list of named gams.
+  # argument fml is a string list stating the family of the conditional prob. and must be provided separately
+  md=list(gm=gam(data = df, formula = y~ t + s(x, t, bs = 'fs', k=3), family = fml, method="REML", select = T))
+}
+
+fml <- list('gaussian', 'gaussian', 'poisson',
+            'betar', 'gaussian')
+
+gms <- mapply(Mygamsc, rs, fml) # iteratively apply Mygamsc
+
+sm <- lapply(gms, summary)%>% # get each model summary
+      #lapply(., '[[', 'p.table')%>% 
+      lapply(., '[[', 's.table')%>% # extract each s.table
+      imap_dfr(., ~ bind_cols(mod = .y,  edf = .x[,1], ref.df = .x[,2], 
+                          Fval = round(.x[,3], 3), pval = round(.x[,4], 3))) # format stats
+
+pm <- lapply(gms, summary)%>% # get each model summary
+      lapply(., '[[', 'p.table')%>%
+      imap_dfr(., ~ bind_cols(mod = .y, term = rownames(.x), bet = round(.x[,1], 3), StdE = round(.x[,2],3), 
+                          tval = round(.x[,3], 3), pval = round(.x[,4], 3))) # format stats
+
+kable(sm, format = "simple", caption = "Table 2", digits = 3) # pretty simple table
+kable(pm, format = "simple", caption = "Table 3", digits = 3) # pretty simple table
+write_csv(sm, "table2") # good old way to save results
+write_csv(sm, "table3") # good old way to save results
+# check model fit with diagnostics 
+#overview(mds$alf.mod)
+check <- function(b, k.sample = 5000, k.rep = 200) {
+  mgcv:::k.check(b, subsample = k.sample, n.rep = k.rep)
+}
+
+# function to check if k is correctly specified
+chs <- lapply(gms, check)%>%
+  imap_dfr(., ~ bind_cols(mod = .y,  k = .x[,1], edf = .x[,2], 
+                          kindex = round(.x[,3], 3), pval = round(.x[,4], 3))) # format stats
+
+appraise(gms$cf.gm, method = "simulate", n_simulate = 1000)
+appraise(gms$mwd.gm, method = "simulate", n_simulate = 1000)
+appraise(gms$imp.gm, method = "simulate", n_simulate = 1000)
+appraise(gms$wsa.gm, method = "simulate", n_simulate = 1000)
+appraise(gms$alf.gm, method = "simulate", n_simulate = 1000)
+
+# Permanovas
+# Multivariate Model (PERMANOVA)
+adonis2(otu_table(f)~f_tr$t, permutations = 999, method = 'jac', binary=T, by="term")
+
+# Plots
+# estimate uncertainty around fits
+Myse <- function(mds, df, i){
+  # function to extract the fit and standard error in the response scale
+  # models are the list of models defined by Mymodsc
+  # df is the list of data.frames that contain the predictor needed to pass to function predict
+  # i is the number of iterations of this routine = number of datasets/models
+  nd <- lapply(df, function(y) with(y, tibble(x = seq(min(x),max(x), length.out=length(x)), t=t)))
+  fit <- lapply(mds, function(x){do.call(cbind, predict(x, nd[[i]], type='response', se.fit=T, unconditional=T))})
+  fit2 <- lapply(fit, function(x){cbind(x, nd[[i]])})
+}
+se <- Myse(mds=gms, df=rs, i=5)
+
+# function for plots
+Myplotsc=function(df, fml, laby, se){
+  
+  colrs <- RColorBrewer::brewer.pal(6, "Set2")
+  plot=list(ggplot(data=df, aes(x=x, y=y))+
+              geom_point(alpha=0.05, color="Blue")+
+              # geom_smooth(data=df, aes(x=x, y=y), method = "gam", formula = y ~ s(x, bs="tp", k=3), 
+              #        method.args=list(family=fml),
+              #        se=T, color="Black")+
+              geom_line(data=se, aes(x=x, y=fit, color=t), linewidth=1.5, inherit.aes = F)+ # use fit estimated by predict
+              #geom_ribbon(data=se,                                  # estimate alpha=95% Credible Intervals
+              #            aes(x=x, ymin = fit- 1.96*(se.fit), ymax = fit + 1.96*(se.fit)), 
+              #            alpha = 0.5, fill = "Grey", inherit.aes = F)+ # this constant comes from qnorm((1-0.95)/2, lower.tail = F)
+              scale_x_continuous(breaks = c(1,3,5,7,9,11,13,15))+
+              scale_color_manual(values=colrs)+
+              labs(y=laby, x="Dryness level", colour="Pollutant ID")+
+              theme(text = element_text(size = 12),
+                    panel.grid.minor = element_blank()))
+}
+
+
+laby <- list('Log of copy number per g of soil', 'Mean weight diameter (mm)', 'Water drop penetration time (sec.)',
+             'Fraction of water stable aggregates', 'Inverse Simpson index (ASVs)')
+
+pl <- mapply(Myplotsc, rs, fml, laby, se)
+
+# Plot ordinations
+# beta diversity (will not go on the list)
+betf <- vegdist(otu_table(f), method = "jac", binary = T)%>%
+  metaMDS(., parallel=2, trace=F, weakties=T)
+
+f_tr <- sample_data(f)[,c(1,2)] %>% data.frame(.) %>% rename(t=treatment) %>%
+        mutate_at('t', as.factor)
+
+ford <- gg_ordiplot(betf, groups = f_tr$t, hull = F, label = F,
+                    spiders = F, ellipse = F, plot = F, choices = c(1, 2), scaling=1)%>%
+  pluck('df_ord')%>%
+  rownames_to_column("potID")%>%
+  mutate_at('potID', as.numeric)%>%
+  left_join(.,f_tr, by="potID") %>% select(-Group)
+
+rm(betf)
+ford <- ford %>% filter(x<1) %>% filter(y<1)
+
+
+colrs <- RColorBrewer::brewer.pal(6, "Set2")
+ord <- ggplot() + 
+  geom_vline(xintercept=0.0, color="White", linewidth=1, linetype=1)+
+  geom_hline(yintercept=0.0, color="White", linewidth=1, linetype=1)+    
+  geom_point(data=ford, aes(x=x, y=y, color=t), alpha=0.8,  size=2, show.legend = T) +
+  scale_color_manual(values=colrs)+
+  labs(x="Axis 1", y="Axis 2", color="Pollutant ID")+
+  theme(axis.title = element_text(size = 12),
+        legend.key = element_blank(),  #removes the box around each legend item
+        legend.position = "right", #legend at the bottom
+        legend.text = element_text(size=12),
+        panel.border = element_rect(colour = "Black", fill = F),
+        panel.grid = element_blank())        
+
+pl$ord <- ord # append ordination plot to list
+
+(figs1 <- ggarrange(pl$mwd, pl$wsa, pl$imp, pl$cf, pl$alf, pl$ord, ncol = 3, nrow = 2,
+                   common.legend = T, legend = "right", labels = c('a', 'b', 'c', 'd', 'e', 'f')))
